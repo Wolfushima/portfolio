3956f9833ce5a46fec2f222a8d9b0aea8fad4288
Adolfo Herrera <valherdolf@gmail.com>
2023-02-21 17:05:31 -0500

    Add Projects section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Home />
         <AboutMe />
+        <Projects />
       </main>
     </>
   );
