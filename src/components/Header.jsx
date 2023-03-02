ee5f7261eaf588c614228720bec1548961664386
Adolfo Herrera <valherdolf@gmail.com>
2023-03-01 21:28:00 -0500

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import scrollToId from '../utils/scrollToId';
 
-const Header = () => (
-  <header id="header" className="header">
-    <div className="header__wrapper">
-      <div className="header__container">
-        <p>Adolfo Herrera</p>
-        <nav>
-          <ul>
-            <li>About Me</li>
-            <li>Portfolio</li>
-            <li>Skills</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
+const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleLiClick = (id) => {
+    scrollToId(id);
+    if (isNavOpen) {
+      setIsNavOpen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <header id="header" className="header">
+      <div className="header__wrapper">
+        <div className="header__container">
+          <button
+            type="button"
+            className="header__logo"
+            onClick={() => scrollToId('home')}
+          >
+            Adolfo Herrera
+          </button>
+          <nav>
+            <ul className={isNavOpen ? 'expanded' : null}>
+              <li>
+                <button type="button" onClick={() => handleLiClick('aboutme')}>
+                  About Me
+                </button>
+              </li>
+              <li>
+                <button type="button" onClick={() => handleLiClick('projects')}>
+                  Projects
+                </button>
+              </li>
+              <li>
+                <button type="button" onClick={() => handleLiClick('skills')}>
+                  Skills
+                </button>
+              </li>
+              <li>
+                <button type="button" onClick={() => handleLiClick('contact')}>
+                  Contact
+                </button>
+              </li>
+            </ul>
+          </nav>
+          <button
+            type="button"
+            className={
+              isNavOpen ? 'header__hamburger open' : 'header__hamburger button'
+            }
+            onClick={toggleNav}
+          >
+            <span />
+            <span />
+            <span />
+            <span />
+          </button>
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
