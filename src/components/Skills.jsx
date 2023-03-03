32b943387b144806962493ae27f552dfacc32f04
Adolfo Herrera <valherdolf@gmail.com>
2023-03-02 20:42:06 -0500

    Update Skills, Add framer motion animations

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import JavaScriptLogo from '../assets/skills/javascript-logo.svg';
 import HTMLLogo from '../assets/skills/html-logo.svg';
 import CSSLogo from '../assets/skills/css-logo.svg';
@@ -10,13 +11,39 @@ import GitHubLogo from '../assets/skills/github-logo.svg';
 import WebpackLogo from '../assets/skills/webpack-logo.svg';
 import FigmaLogo from '../assets/skills/figma-logo.svg';
 
+const boxVariants = {
+  visible: { opacity: 1, scale: 1 },
+  hidden: { opacity: 0, scale: 0 },
+};
+
 const Skill = ({ logo, skillName }) => (
   <div className="skills__skill">
     <div className="skills__logo">
-      <img src={logo} alt={skillName} />
+      <motion.img
+        src={logo}
+        alt={skillName}
+        whileHover={{
+          scale: 1.1,
+          transition: { duration: 0.3 },
+        }}
+      />
     </div>
     <p>{skillName}</p>
   </div>
+);
+
+const SkillBox = ({ skillBoxTitle, children }) => (
+  <motion.div
+    className="skills__box"
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true }}
+    transition={{ type: 'tween', duration: 0.7 }}
+    variants={boxVariants}
+  >
+    <h5>{skillBoxTitle}</h5>
+    <div className="skills__box-items">{children}</div>
+  </motion.div>
 );
 
 const Skills = () => (
@@ -27,36 +54,24 @@ const Skills = () => (
           <h3>SKILLS</h3>
         </div>
         <div className="skills__skills">
-          <div className="skills__box">
-            <h5>Languages</h5>
-            <div className="skills__box-items">
-              <Skill skillName="JavaScript" logo={JavaScriptLogo} />
-              <Skill skillName="HTML" logo={HTMLLogo} />
-              <Skill skillName="CSS" logo={CSSLogo} />
-              <Skill skillName="Sass" logo={SassLogo} />
-            </div>
-          </div>
-          <div className="skills__box">
-            <h5>Frameworks</h5>
-            <div className="skills__box-items">
-              <Skill skillName="React" logo={ReactLogo} />
-              <Skill skillName="Bootstrap" logo={BootstrapLogo} />
-            </div>
-          </div>
-          <div className="skills__box">
-            <h5>Collaborative Work</h5>
-            <div className="skills__box-items">
-              <Skill skillName="Git" logo={GitLogo} />
-              <Skill skillName="GitHub" logo={GitHubLogo} />
-            </div>
-          </div>
-          <div className="skills__box">
-            <h5>Tools</h5>
-            <div className="skills__box-items">
-              <Skill skillName="Webpack" logo={WebpackLogo} />
-              <Skill skillName="Figma" logo={FigmaLogo} />
-            </div>
-          </div>
+          <SkillBox skillBoxTitle="Languages">
+            <Skill skillName="JavaScript" logo={JavaScriptLogo} />
+            <Skill skillName="HTML" logo={HTMLLogo} />
+            <Skill skillName="CSS" logo={CSSLogo} />
+            <Skill skillName="Sass" logo={SassLogo} />
+          </SkillBox>
+          <SkillBox skillBoxTitle="Frameworks">
+            <Skill skillName="React" logo={ReactLogo} />
+            <Skill skillName="Bootstrap" logo={BootstrapLogo} />
+          </SkillBox>
+          <SkillBox skillBoxTitle="Collaborative Work">
+            <Skill skillName="Git" logo={GitLogo} />
+            <Skill skillName="GitHub" logo={GitHubLogo} />
+          </SkillBox>
+          <SkillBox skillBoxTitle="Tools">
+            <Skill skillName="Webpack" logo={WebpackLogo} />
+            <Skill skillName="Figma" logo={FigmaLogo} />
+          </SkillBox>
         </div>
       </div>
     </div>
