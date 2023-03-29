c8cce7dff0fabf2fdcfc0652076e2a034f1fd6bf
Adolfo Herrera <valherdolf@gmail.com>
2023-03-28 20:24:45 -0400

    Update Contact, add form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import EmailIcon from '../assets/contact/email-icon.svg';
 
-const Contact = () => (
-  <section id="contact" className="contact">
-    <div className="contact__wrapper">
-      <div className="contact__container">
-        <div className="contact__header">
-          <h3>CONTACT</h3>
-        </div>
-        <div className="contact__msg">
-          <p>I&apos;m currently available for work</p>
-        </div>
-        <div className="contact__button">
-          <button type="button">
-            <img src={EmailIcon} alt="Email" />
-            <p>Email Me</p>
-          </button>
+const Contact = () => {
+  return (
+    <section id="contact" className="contact">
+      <div className="contact__wrapper">
+        <div className="contact__container">
+          <div className="contact__header">
+            <h3>CONTACT</h3>
+          </div>
+          <div className="contact__msg">
+            <p>I&apos;m currently available for work</p>
+          </div>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            className="contact__form"
+          >
+            <div>
+              <label htmlFor="name">
+                Your Name: <input type="text" name="name" />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="email">
+                Your Email: <input type="email" name="email" />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="message">
+                Message: <textarea name="message" />
+              </label>
+            </div>
+            <div className="contact__button">
+              <button type="submit">
+                <img src={EmailIcon} alt="Email" />
+                <p>Email Me</p>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Contact;
