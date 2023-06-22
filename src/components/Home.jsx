94195778847729b2380b30e34c56f203ce084160
Adolfo Herrera <valherdolf@gmail.com>
2023-06-22 17:11:03 -0400

    fix: Remove framer motion props from home__center and added them to their respective container

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,50 +10,71 @@ const homeVariants = {
   visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
 };
 
+const HomeHeaders = () => (
+  <motion.div
+    className="home__headers"
+    initial="hidden"
+    animate="visible"
+    exit={{ opacity: 0, transition: { duration: 1 } }}
+    variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+  >
+    <motion.p variants={homeVariants}>Hi, I&apos;m</motion.p>
+    <motion.h1 variants={homeVariants}>Adolfo Herrera</motion.h1>
+    <motion.h2 variants={homeVariants}>Frontend Developer</motion.h2>
+  </motion.div>
+);
+
+const HomeContact = () => (
+  <motion.div
+    className="home__contact"
+    initial="hidden"
+    animate="visible"
+    exit={{ opacity: 0, transition: { duration: 1 } }}
+    variants={{
+      visible: {
+        transition: {
+          delayChildren: 1,
+          staggerChildren: 0.3,
+        },
+      },
+    }}
+  >
+    <motion.a
+      variants={homeVariants}
+      href="https://www.linkedin.com/in/adolfoherreradev/"
+      className="home__contact-logo"
+      aria-label="LinkedIn"
+    >
+      <LinkedInLogo />
+    </motion.a>
+    <motion.a
+      variants={homeVariants}
+      href="https://github.com/Wolfushima"
+      className="home__contact-logo"
+      aria-label="GitHub"
+    >
+      <GitHubLogo />
+    </motion.a>
+    <motion.button
+      variants={homeVariants}
+      type="button"
+      className="home__contact-logo"
+      onClick={() => scrollToId('contact')}
+      aria-label="Email"
+    >
+      <EmailIcon />
+    </motion.button>
+  </motion.div>
+);
+
 const Home = () => (
   <section id="home" className="home">
     <div className="home__wrapper">
       <div className="home__container">
-        <motion.div
-          className="home__center"
-          initial="hidden"
-          animate="visible"
-          exit={{ opacity: 0, transition: { duration: 1 } }}
-          variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
-        >
-          <div className="home__headers">
-            <motion.p variants={homeVariants}>Hi, I&apos;m</motion.p>
-            <motion.h1 variants={homeVariants}>Adolfo Herrera</motion.h1>
-            <motion.h2 variants={homeVariants}>Frontend Developer</motion.h2>
-          </div>
-          <div className="home__contact">
-            <motion.a
-              variants={homeVariants}
-              href="https://www.linkedin.com/in/adolfoherreradev/"
-              className="home__contact-logo"
-              aria-label="LinkedIn"
-            >
-              <LinkedInLogo />
-            </motion.a>
-            <motion.a
-              variants={homeVariants}
-              href="https://github.com/Wolfushima"
-              className="home__contact-logo"
-              aria-label="GitHub"
-            >
-              <GitHubLogo />
-            </motion.a>
-            <motion.button
-              variants={homeVariants}
-              type="button"
-              className="home__contact-logo"
-              onClick={() => scrollToId('contact')}
-              aria-label="Email"
-            >
-              <EmailIcon />
-            </motion.button>
-          </div>
-        </motion.div>
+        <div className="home__center">
+          <HomeHeaders />
+          <HomeContact />
+        </div>
         <div className="home__shape-divider-bottom">
           <svg
             data-name="Layer 1"
