69913d7141232c2aa5ad720cc8b81ae3dd566083
Adolfo Herrera <valherdolf@gmail.com>
2023-07-05 17:23:55 -0400

    style: Import layered-waves-haikei.svg to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { ReactComponent as LinkedInLogoSVG } from '../assets/footer/linkedin-logo.svg';
 import { ReactComponent as GitHubLogoSVG } from '../assets/skills/github-logo.svg';
 import { ReactComponent as InstagramLogoSVG } from '../assets/footer/instagram-logo.svg';
+import { ReactComponent as LayeredWavesSVG } from '../assets/home/layered-waves-haikei.svg';
 
 const Footer = () => (
   <footer id="footer" className="footer">
+    <LayeredWavesSVG className="footer__layered-waves" />
     <div className="footer__wrapper">
       <div className="footer__container">
         <div className="footer__author">
