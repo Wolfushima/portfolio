7279f3f34c00cf4749198d8cd7408c8e8c8b8fba
Adolfo Herrera <valherdolf@gmail.com>
2023-07-06 20:55:34 -0400

    style: Update layout for Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import EmailIcon from '../assets/contact/email-icon.svg';
+import { ReactComponent as EmailIcon } from '../assets/contact/email-icon.svg';
 
 const Contact = () => {
   return (
     <section id="contact" className="contact">
       <div className="contact__wrapper">
         <div className="contact__container">
-          <div className="contact__header">
-            <h3>CONTACT</h3>
-          </div>
-          <div className="contact__msg">
-            <p>I&apos;m currently available for work</p>
+          <div className="contact__left">
+            <div className="contact__header">
+              <h3>CONTACT</h3>
+            </div>
+            <div className="contact__msg">
+              <p>Let&apos;s get in touch.</p>
+              <p>
+                I am actively seeking new work opportunities and currently
+                available for hire.
+              </p>
+            </div>
           </div>
           <form
             className="contact__form"
@@ -37,7 +43,7 @@ const Contact = () => {
             </div>
             <div className="contact__button">
               <button type="submit">
-                <img src={EmailIcon} alt="Email" />
+                <EmailIcon />
                 <p>Email Me</p>
               </button>
             </div>
